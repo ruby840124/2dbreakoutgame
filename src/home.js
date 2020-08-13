0b61f52a0cfa194de6b08e5d9609c43d003538b8
pft\\Ruby_Feng <Ruby_Feng@PerfectCorp.com>
2020-08-13 18:32:34 +0800

    'snapshot'

--- src/home.js
+++ src/home.js
@@ -12,15 +12,31 @@ class home extends React.Component{
     //this.addComponentToCanvas();
   }
 
+  isMobile = () => {
+    try { 
+      document.createEvent("TouchEvent"); 
+      return true;
+    } catch(e) {
+       return false;
+    }
+  
+  }
+
   render() {
+    const isMobile = this.isMobile();
     return (
     <div className="App">
-      <p style={{fontSize:"20px"}}>2D breakout game home</p>
-      <div className="home">
-        <Link to="/game"><button><FaCat/>play</button></Link>
-        <Link to="/setting"><button><IoMdSettings/>settings</button></Link>
-        <Link to="/developer"><button><IoIosPerson/>developer</button></Link>
+      {isMobile && 'not support mobile'}
+      {!isMobile && 
+      <div className="App">
+        <p style={{fontSize:"20px"}}>2D breakout game home</p>
+        <div className="home">
+          <Link to="/game"><button><FaCat/>play</button></Link>
+          <Link to="/setting"><button><IoMdSettings/>settings</button></Link>
+          <Link to="/developer"><button><IoIosPerson/>developer</button></Link>
+        </div>
       </div>
+      }
     </div>
     )
   }
