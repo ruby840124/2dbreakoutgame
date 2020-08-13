87814386c0ad9dbc48724283134f0141772451f1
pft\\Ruby_Feng <Ruby_Feng@PerfectCorp.com>
2020-08-13 17:43:09 +0800

    Updates

---
+++ precache-manifest.5743269988facaec0ac99656128dbd29.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb4314655b6b375c52f778a23b57812",
+    "url": "/2dbreakoutgame/index.html"
+  },
+  {
+    "revision": "7a3c0831304074536cd7",
+    "url": "/2dbreakoutgame/static/css/2.b00f4da7.chunk.css"
+  },
+  {
+    "revision": "aea21fe60220e3b5eef5",
+    "url": "/2dbreakoutgame/static/css/main.1bf5f61c.chunk.css"
+  },
+  {
+    "revision": "7a3c0831304074536cd7",
+    "url": "/2dbreakoutgame/static/js/2.f0931f98.chunk.js"
+  },
+  {
+    "revision": "f247106060c2ad7059e08c5a808c40e2",
+    "url": "/2dbreakoutgame/static/js/2.f0931f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea21fe60220e3b5eef5",
+    "url": "/2dbreakoutgame/static/js/main.f9de041b.chunk.js"
+  },
+  {
+    "revision": "3cdf135c198f74354831",
+    "url": "/2dbreakoutgame/static/js/runtime-main.84c11f3e.js"
+  },
+  {
+    "revision": "07ece49a08ff11e1567bd6ea196c052a",
+    "url": "/2dbreakoutgame/static/media/anders.07ece49a.JPG"
+  },
+  {
+    "revision": "bfa1f1114eb942b6862bd3aa73fc0081",
+    "url": "/2dbreakoutgame/static/media/background.bfa1f111.jpg"
+  },
+  {
+    "revision": "50b1feaaa97281ef704993f776f3c4a0",
+    "url": "/2dbreakoutgame/static/media/cat.50b1feaa.gif"
+  },
+  {
+    "revision": "ca76ff1ecad8ddca5484f8a290d6cbed",
+    "url": "/2dbreakoutgame/static/media/gary.ca76ff1e.JPG"
+  },
+  {
+    "revision": "faa9eaaa29fd3c51afd66343ff11a279",
+    "url": "/2dbreakoutgame/static/media/hero.faa9eaaa.JPG"
+  },
+  {
+    "revision": "85bd92296b1dc61fdaf3a3ff2b3d9cf6",
+    "url": "/2dbreakoutgame/static/media/minray.85bd9229.JPG"
+  },
+  {
+    "revision": "20c3000230a26609f74c797459d17ac7",
+    "url": "/2dbreakoutgame/static/media/ruby.20c30002.png"
+  }
+]);
